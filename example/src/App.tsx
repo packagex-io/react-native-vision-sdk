5b3913ea6f787ab81d21ad51fce7f82f2ed0a74d
Ali Khan <ali.khan@packagex.io>
2023-01-02 17:37:37 +0500

    fix(App): visionsdk fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,5 +2,19 @@ import * as React from 'react';
 import VisionSdkView from 'react-native-vision-sdk';
 
 export default function App() {
-  return <VisionSdkView />;
+  return (
+    <VisionSdkView
+      children={undefined}
+      refProp={undefined}
+      BarCodeScanHandler={function (_e: any): void {
+        throw new Error('Function not implemented.');
+      }}
+      OCRScanHandler={function (_e: any): void {
+        throw new Error('Function not implemented.');
+      }}
+      OnDetectedHandler={function (_e: any): void {
+        throw new Error('Function not implemented.');
+      }}
+    />
+  );
 }
