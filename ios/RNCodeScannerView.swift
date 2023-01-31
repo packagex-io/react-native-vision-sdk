31486f7edd6f34a8c8c1208de5a35650cec02e7d
Ali Khan <ali.khan@packagex.io>
2023-01-31 14:17:06 +0500

    feat: vision sdk ios update

--- ios/RNCodeScannerView.swift
+++ ios/RNCodeScannerView.swift
@@ -1,34 +1,30 @@
 import UIKit
 import VisionSDK
 
+class RNCodeScannerView: UIView, CodeScannerViewDelegate {
 
-class RNCodeScannerView : UIView, CodeScannerViewDelegate {
-    
   // events from swift to Js
-  @objc var onBarcodeScanSuccess : RCTDirectEventBlock?
-  
-  @objc var onOCRDataReceived : RCTDirectEventBlock?
+  @objc var onBarcodeScanSuccess: RCTDirectEventBlock?
 
-  @objc var onDetected : RCTDirectEventBlock?
-  
-  var codeScannerView : CodeScannerView?
-  @objc var onError : RCTDirectEventBlock?
+  @objc var onOCRDataReceived: RCTDirectEventBlock?
 
+  @objc var onDetected: RCTDirectEventBlock?
 
+  var codeScannerView: CodeScannerView?
+  @objc var onError: RCTDirectEventBlock?
 
-  
   //prop from Js to Swift
   @objc func setMode(_ mode: NSString) {
-    
+
     if codeScannerView == nil {
       return
     }
-    
-    if (mode == "ocr") {
+
+    if mode == "ocr" {
       codeScannerView!.setScanModeTo(.ocr)
       codeScannerView!.isBarCodeOrQRCodeIndicationOn = true
-      
-    } else if (mode == "barcode") {
+
+    } else if mode == "barcode" {
       codeScannerView!.setScanModeTo(.barCode)
       codeScannerView!.isBarCodeOrQRCodeIndicationOn = true
 
@@ -37,177 +33,178 @@ class RNCodeScannerView : UIView, CodeScannerViewDelegate {
       codeScannerView!.isBarCodeOrQRCodeIndicationOn = true
     }
   }
-//    prop from Js to Swift
-//   @objc func setautoMode(_ autoMode: NSString) {
-////     if codeScannerView == nil {
-////       return
-////     }
-////       codeScannerView?.setCaptureModeTo(.auto)
-////    if (autoMode == "auto") {
-////       codeScannerView!.setCaptureModeTo(.auto)
-////    }else{
-////        codeScannerView!.setCaptureModeTo(.manual)
-////    }
-//   }
+  //    prop from Js to Swift
+  //   @objc func setautoMode(_ autoMode: NSString) {
+  ////     if codeScannerView == nil {
+  ////       return
+  ////     }
+  ////       codeScannerView?.setCaptureModeTo(.auto)
+  ////    if (autoMode == "auto") {
+  ////       codeScannerView!.setCaptureModeTo(.auto)
+  ////    }else{
+  ////        codeScannerView!.setCaptureModeTo(.manual)
+  ////    }
+  //   }
 
-    @objc func setCaptureMode(_ captureMode: NSString){
-        if(captureMode == "auto"){
-            codeScannerView?.setCaptureModeTo(.auto)
-        }else{
-            codeScannerView?.setCaptureModeTo(.manual)
-        }
+  @objc func setCaptureMode(_ captureMode: NSString) {
+    if captureMode == "auto" {
+      codeScannerView?.setCaptureModeTo(.auto)
+    } else {
+      codeScannerView?.setCaptureModeTo(.manual)
     }
-//  prop from Js to Swift
-//   @objc func setAPIKey(_ apiKey: NSString) {
-//     Constants.apiKey = apiKey as String
-//   }
-    @objc func setApiKey(_ apiKey: NSString){
-        Constants.apiKey = apiKey as String
-    }
-  
-    func codeScannerView(_ scannerView: VisionSDK.CodeScannerView, didSuccess code: [String]) {
+  }
+  //  prop from Js to Swift
+  //   @objc func setAPIKey(_ apiKey: NSString) {
+  //     Constants.apiKey = apiKey as String
+  //   }
+  @objc func setApiKey(_ apiKey: NSString) {
+    Constants.apiKey = apiKey as String
+  }
+
+  func codeScannerView(_ scannerView: VisionSDK.CodeScannerView, didSuccess code: [String]) {
     if onBarcodeScanSuccess != nil {
       onBarcodeScanSuccess!(["code": code])
       scannerView.rescan()
     }
   }
-    
-  func codeScannerView(_ scannerView: VisionSDK.CodeScannerView, didFailure error: VisionSDK.CodeScannerError) {
-//          onError!(["message":error]);
-    }
-  
+
+  func codeScannerView(
+    _ scannerView: VisionSDK.CodeScannerView, didFailure error: VisionSDK.CodeScannerError
+  ) {
+    //          onError!(["message":error]);
+  }
+
   func codeScannerViewDidDetect(_ text: Bool, barCode: Bool, qrCode: Bool) {
     if onDetected != nil {
-      onDetected!(["text" : text, "barCode" : barCode, "qrCode" : qrCode])
+      onDetected!(["text": text, "barCode": barCode, "qrCode": qrCode])
     }
   }
-  
-  func codeScannerView(_ scannerView: CodeScannerView, didCaptureOCRImage image: UIImage, withbarCodes barcodes: [String]) {
-    self.callOCRAPIWithImage(image, andBarcodes: barcodes)
 
+  func codeScannerView(
+    _ scannerView: CodeScannerView, didCaptureOCRImage image: UIImage,
+    withbarCodes barcodes: [String], andToken token: [String], andLocationId locationId: [String],
+    andOptions options: [String: String]
+  ) {
+    self.callOCRAPIWithImage(
+      image, andBarcodes: barcodes, andToken: token, andLocationId: locationId, andOptions: options)
   }
-  
+
   func callForOCRWithImageInProgress() {
     // do stuff while vision api call is in progress
   }
-  
-  func callForOCRWithImageCompletedWithData(data: Dictionary<AnyHashable, Any>){
+
+  func callForOCRWithImageCompletedWithData(data: [AnyHashable: Any]) {
     if onOCRDataReceived != nil {
-      onOCRDataReceived!(["data" : data])
-    }
-    else {
-            onError!(["message":"not found"]);
+      onOCRDataReceived!(["data": data])
+    } else {
+      onError!(["message": "not found"])
     }
   }
-  
-  func callForOCRWithImageFailedWithMessage(message: String){
-          onError!(["message":message]);
-     
+
+  func callForOCRWithImageFailedWithMessage(message: String) {
+    onError!(["message": message])
+
   }
-  
-  
+
   init() {
     super.init(frame: UIScreen.main.bounds)
-    
-   codeScannerView = CodeScannerView(frame: UIScreen.main.bounds)
-    
 
-    codeScannerView!.configure(delegate: self, input: .init(focusImage: nil, shouldDisplayFocusImage: true, shouldScanInFocusImageRect: true, isTextIndicationOn: true, isBarCodeOrQRCodeIndicationOn: true, sessionPreset: .high, nthFrameToProcess: 10, captureMode: .manual, captureType: .single), scanMode: .barCode)
+    codeScannerView = CodeScannerView(frame: UIScreen.main.bounds)
 
-
+    codeScannerView!.configure(
+      delegate: self,
+      input: .init(
+        focusImage: nil, shouldDisplayFocusImage: true, shouldScanInFocusImageRect: true,
+        isTextIndicationOn: true, isBarCodeOrQRCodeIndicationOn: true, sessionPreset: .high,
+        nthFrameToProcess: 10, captureMode: .manual, captureType: .single), scanMode: .barCode)
 
     codeScannerView!.startRunning()
 
     self.addSubview(codeScannerView!)
   }
-  
 
   required init?(coder aDecoder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
-    }
-
-
+    fatalError("init(coder:) has not been implemented")
+  }
 }
-
-
-
 
 extension RNCodeScannerView {
-    
-    private func callOCRAPIWithImage(_ image: UIImage, andBarcodes barcodes: [String]) {
-        
-        self.callForOCRWithImageInProgress()
-        
-//        VisionAPIManager.shared.callScanAPIWith(image, andBarcodes: barcodes, withMailroomId: 777644, andApiKey: "XRI8HaRVOM5P9OS2Mhakq3voRSOs66hK6j7CSWUG") {
-//
-//            [weak self] data, response, error in
-      
-        VisionAPIManager.shared.callScanAPIWith(image, andBarcodes: barcodes, andApiKey: !Constants.apiKey.isEmpty ? Constants.apiKey : "key_stag_7da7b5e917tq2eCckhc5QnTr1SfpvFGjwbTfpu1SQYy242xPjBz2mk3hbtzxVw1zj5K5XBF") {
 
-              [weak self] data, response, error in
-            
-            guard let self = self else { return }
-            
-            // Check if there's an error or response data is nil
-            guard error == nil else {
-                DispatchQueue.main.async {
-                  self.callForOCRWithImageFailedWithMessage(message: error?.localizedDescription ?? "")
-                }
-                
-                return
-            }
-            
-            guard let response = response else {
-                DispatchQueue.main.async {
-                  self.callForOCRWithImageFailedWithMessage(message: "Response of request was found nil")
-                }
-                return
-            }
-            
-            guard let data = data else {
-                DispatchQueue.main.async {
-                  self.callForOCRWithImageFailedWithMessage(message: "Data of request was found nil")
-                }
-                return
-            }
-             
-          _ = (response as! HTTPURLResponse).statusCode
-           
-            
-            DispatchQueue.main.async {
-                
-                do {
-                    
-                    if let jsonResponse = try JSONSerialization.jsonObject(with: data) as? [String: Any], let responseJson = jsonResponse["data"] {
-                                                
-                      if (try? JSONSerialization.data(withJSONObject: responseJson)) != nil {
-                        self.callForOCRWithImageCompletedWithData(data: jsonResponse)
-                     }
-                    }
-                }
-                catch let error {
-                    
-                   
-                    do {
-                        // create json object from data or use JSONDecoder to convert to Model stuct
-                        if let jsonResponse = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-                            if let message = jsonResponse["message"] as? String {
-                              self.callForOCRWithImageFailedWithMessage(message: message)
-                            }
-                            else {
-                              self.callForOCRWithImageFailedWithMessage(message: error.localizedDescription)
-                            }
+  private func callOCRAPIWithImage(
+    _ image: UIImage, andBarcodes barcodes: [String], andToken token: [String],
+    andLocationId locationId: [String], andOptions options: [String: String]
+  ) {
 
-                        } else {
-                            throw URLError(.badServerResponse)
-                        }
-                    } catch let error {
-                      self.callForOCRWithImageFailedWithMessage(message: error.localizedDescription)
-                    }
-                }
-                
-            }
+    self.callForOCRWithImageInProgress()
+
+    VisionAPIManager.shared.callScanAPIWith(
+      image, andBarcodes: barcodes, andApiKey: !Constants.apiKey.isEmpty ? Constants.apiKey : nil,
+      andToken: token, andLocationId: locationId, andOptions: options
+    ) {
+
+      [weak self] data, response, error in
+
+      guard let self = self else { return }
+
+      // Check if there's an error or response data is nil
+      guard error == nil else {
+        DispatchQueue.main.async {
+          self.callForOCRWithImageFailedWithMessage(message: error?.localizedDescription ?? "")
         }
-    }
-}
 
+        return
+      }
+
+      guard let response = response else {
+        DispatchQueue.main.async {
+          self.callForOCRWithImageFailedWithMessage(message: "Response of request was found nil")
+        }
+        return
+      }
+
+      guard let data = data else {
+        DispatchQueue.main.async {
+          self.callForOCRWithImageFailedWithMessage(message: "Data of request was found nil")
+        }
+        return
+      }
+
+      _ = (response as! HTTPURLResponse).statusCode
+
+      DispatchQueue.main.async {
+
+        do {
+
+          if let jsonResponse = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+            let responseJson = jsonResponse["data"]
+          {
+
+            if (try? JSONSerialization.data(withJSONObject: responseJson)) != nil {
+              self.callForOCRWithImageCompletedWithData(data: jsonResponse)
+            }
+          }
+        } catch let error {
+
+          do {
+            // create json object from data or use JSONDecoder to convert to Model stuct
+            if let jsonResponse = try JSONSerialization.jsonObject(
+              with: data, options: .mutableContainers) as? [String: Any]
+            {
+              if let message = jsonResponse["message"] as? String {
+                self.callForOCRWithImageFailedWithMessage(message: message)
+              } else {
+                self.callForOCRWithImageFailedWithMessage(message: error.localizedDescription)
+              }
+
+            } else {
+              throw URLError(.badServerResponse)
+            }
+          } catch let error {
+            self.callForOCRWithImageFailedWithMessage(message: error.localizedDescription)
+          }
+        }
+
+      }
+    }
+  }
+}
--- ios/VisionSdkViewManager.swift
+++ ios/VisionSdkViewManager.swift
@@ -2,32 +2,28 @@ import VisionSDK
 
 @objc(VisionSdkViewManager)
 class VisionSdkViewManager: RCTViewManager {
-    
-    
-    
-    @objc func captureImage(_ node: NSNumber) {
-        
-        DispatchQueue.main.async {
-          let component = self.bridge.uiManager.view(
-            forReactTag: node
-          ) as! RNCodeScannerView
-          
-          component.codeScannerView!.capturePhoto()
-          
-          
-      }
-      
-  }
-    
-   
-    override func view() -> UIView! {
-        
-      
-      return RNCodeScannerView()
-    
+
+  @objc func captureImage(_ node: NSNumber) {
+
+    DispatchQueue.main.async {
+      let component =
+        self.bridge.uiManager.view(
+          forReactTag: node
+        ) as! RNCodeScannerView
+
+      component.codeScannerView!.capturePhoto()
+
     }
-    
-    override static func requiresMainQueueSetup() -> Bool {
-        return true
-      }
+
+  }
+
+  override func view() -> UIView! {
+
+    return RNCodeScannerView()
+
+  }
+
+  override static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
 }
