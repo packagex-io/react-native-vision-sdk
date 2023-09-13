5ca57630cfa52a4ad704b031d4e975e1bbfb9cd9
Muhammad Sheharyar Khan <muhammadsheharyarkhan@PX-LT-0233.local>
2023-09-13 12:21:13 +0500

    lint fix

--- src/index.tsx
+++ src/index.tsx
@@ -30,10 +30,10 @@ const Camera: React.FC<Props> = ({
   refProp,
   // key,
   reRender,
-  BarCodeScanHandler = (_e: any) => { },
-  OCRScanHandler = (_e: any) => { },
-  OnDetectedHandler = (_e: any) => { },
-  onError = (_e: any): void => { },
+  BarCodeScanHandler = (_e: any) => {},
+  OCRScanHandler = (_e: any) => {},
+  OnDetectedHandler = (_e: any) => {},
+  onError = (_e: any): void => {},
 }: Props) => {
   const defaultScanMode = ScanMode.BARCODE;
   const [mode, setMode] = useState<ScanMode>(defaultScanMode);
@@ -89,7 +89,7 @@ const Camera: React.FC<Props> = ({
       (UIManager.hasViewManagerConfig('VisionSDKView') &&
         UIManager.getViewManagerConfig('VisionSDKView').Commands
           .captureImage) ||
-      0,
+       0,
       []
     );
   };
@@ -100,7 +100,7 @@ const Camera: React.FC<Props> = ({
       findNodeHandle(VisionSDKViewRef.current),
       (UIManager.hasViewManagerConfig('VisionSDKView') &&
         UIManager.getViewManagerConfig('VisionSDKView').Commands.stopRunning) ||
-      1,
+       1,
       []
     );
   };
@@ -112,7 +112,7 @@ const Camera: React.FC<Props> = ({
       (UIManager.hasViewManagerConfig('VisionSDKView') &&
         UIManager.getViewManagerConfig('VisionSDKView').Commands
           .startRunning) ||
-      2,
+       2,
       []
     );
   };
@@ -149,9 +149,7 @@ const Camera: React.FC<Props> = ({
         mode={mode}
         captureMode={cameraCaptureMode}
         onError={onError}
-        token={
-          token
-        }
+        token={token}
         locationId={locationId}
         options={Platform.OS === 'ios' ? options : JSON.stringify(options)} // ideally this should be passed from options variable, that is receiving data from ScannerContainer
         environment={environment}
