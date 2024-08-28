c465272efff65af8b6612fddcffa5e5f9c963fff
Muhammad Sheharyar Khan <77493067+msheharyark@users.noreply.github.com>
2024-08-28 12:11:00 +0500

    declaration check handling

--- example/src/types/declarations.d.ts
+++ example/src/types/declarations.d.ts
@@ -1,14 +1,4 @@
-// src/types/declarations.d.ts
-
 declare module 'react-native-vector-icons/Ionicons' {
-  const content: any;
-  export default content;
-}
-declare module 'react-native-vector-icons/MaterialCommunityIcons' {
-  const content: any;
-  export default content;
-}
-declare module 'react-native-vector-icons/FontAwesome' {
-  const content: any;
-  export default content;
+  const Ionicons: any;
+  export default Ionicons;
 }
