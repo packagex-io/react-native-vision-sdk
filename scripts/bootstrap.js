60db3c6fdf05825a43673cb240420c90969f6c83
Muhammad Sheharyar Khan <77493067+msheharyark@users.noreply.github.com>
2024-10-08 11:18:13 +0500

    fix:eslint

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,6 +1,11 @@
-const os = require('os');
-const path = require('path');
-const child_process = require('child_process');
+import os from 'os';
+import path from 'path';
+import { spawnSync } from 'child_process';
+import { fileURLToPath } from 'url';
+
+// Get __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const root = path.resolve(__dirname, '..');
 const args = process.argv.slice(2);
@@ -20,10 +25,10 @@ let result;
 if (process.cwd() !== root || args.length) {
   // We're not in the root of the project, or additional arguments were passed
   // In this case, forward the command to `yarn`
-  result = child_process.spawnSync('yarn', args, options);
+  result = spawnSync('yarn', args, options);
 } else {
   // If `yarn` is run without arguments, perform bootstrap
-  result = child_process.spawnSync('yarn', ['bootstrap'], options);
+  result = spawnSync('yarn', ['bootstrap'], options);
 }
 
 process.exitCode = result.status;
