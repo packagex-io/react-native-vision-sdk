bb33231d9a1c9e82bae02a6284d4c26925b9166f
Kashif Mehmood <kashif.mehmood@packagex.io>
2026-03-19 16:42:40 +0500

    fix: enable multiple scan in photo mode and bump Android SDK to v2.4.39

--- android/src/newarch/java/com/visionsdk/VisionCameraViewManager.kt
+++ android/src/newarch/java/com/visionsdk/VisionCameraViewManager.kt
@@ -270,22 +270,6 @@ class VisionCameraViewManager(private val appContext: ReactApplicationContext) :
 
     private fun applyScanArea(view: VisionCameraView, scanArea: com.facebook.react.bridge.ReadableMap?) {
         try {
-            // Photo mode doesn't use scan areas or focus rects, but still needs FocusSettings
-            // for showNativeBoundingBoxes to work
-            if (currentDetectionMode == DetectionMode.Photo) {
-                Log.d(TAG, "Photo mode - applying FocusSettings without scan area")
-                val focusSettings = io.packagex.visionsdk.config.FocusSettings(
-                    context = appContext,
-                    showCodeBoundariesInMultipleScan = showNativeBoundingBoxes,
-                    showDocumentBoundaries = false,
-                    validCodeBoundaryBorderColor = 0xFFFFEB3B.toInt(), // Yellow (#FFEB3B)
-                    validCodeBoundaryBorderWidth = 2,
-                    validCodeBoundaryFillColor = android.graphics.Color.TRANSPARENT,
-                )
-                view.getFocusRegionManager()?.setFocusSettings(focusSettings)
-                return
-            }
-
             if (scanArea != null) {
                 // When scan area is defined, disable multiple scan mode
                 view.setMultipleScanEnabled(false)
